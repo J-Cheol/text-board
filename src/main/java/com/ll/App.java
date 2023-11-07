cfac267d52722ad8a4aeef8cddffdd527ad90f8d
J-Cheol <lunaris0206@naver.com>
2023-11-07 23:49:49 +0900

    readme create

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,9 +1,53 @@
 package com.ll;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class App
 {
-    void run()
+    private final Scanner scanner;
+
+    public App(Scanner scanner)
     {
-        System.out.println("hi");
+        this.scanner = scanner;
+    }
+
+    public void run()
+    {
+        System.out.println("== 명언 앱 ==");
+        int id = 0;
+        List<Text_List> 목록 = new ArrayList<>();
+        while (true)
+        {
+            System.out.print("명령) ");
+            String cmd = scanner.next();
+            if(cmd.equalsIgnoreCase("종료"))
+                break;
+            if(cmd.equalsIgnoreCase("등록"))
+            {
+                scanner.nextLine();
+                System.out.print("명언 : ");
+                String 명언 = scanner.nextLine();
+                System.out.print("작가 : ");
+                String 작가 = scanner.nextLine();
+
+                id++;
+                Text_List text_list = new Text_List(id, 작가, 명언);
+                목록.add(text_list);
+                System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+            }
+            if(cmd.equalsIgnoreCase("목록"))
+            {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+                for (int i = 목록.size() - 1; i >= 0; i--)
+                {
+                    Text_List text_list = 목록.get(i);
+                    System.out.printf("%d / %s / %s\n", text_list.id, text_list.작가, text_list.명언);
+                }
+            }
+        }
+
     }
 }
---
+++ src/main/java/com/ll/Text_List.java
@@ -0,0 +1,15 @@
+package com.ll;
+
+public class Text_List
+{
+    int id;
+    String 명언;
+    String 작가;
+    Text_List(int id, String 작가, String 명언)
+    {
+        this.id = id;
+        this.작가 = 작가;
+        this.명언 = 명언;
+    }
+
+}
